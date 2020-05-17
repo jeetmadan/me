565781148dc94d3df26bd82d6ab1cd806bbfcbd0
Madanjeet Kumar <kumarmadanjeet1997@gmail.com>
2020-05-17 13:18:26 +0530

    Updates

---
+++ precache-manifest.1265aa8514b4bb255004089b625d4b41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93674e99f430f9a1afc0629403101ee8",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "715e12827fa282b15e26",
+    "url": "/me/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "e910e7be7ac74a2a0d77",
+    "url": "/me/static/js/2.07466bd5.chunk.js"
+  },
+  {
+    "revision": "f24d0ac7eb301a6921c7320a3b841b8e",
+    "url": "/me/static/js/2.07466bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715e12827fa282b15e26",
+    "url": "/me/static/js/main.fd311953.chunk.js"
+  },
+  {
+    "revision": "300a927be0c6c06b4a9d",
+    "url": "/me/static/js/runtime-main.ae24c08c.js"
+  },
+  {
+    "revision": "eab9224b540296eab2adbfd353f46f75",
+    "url": "/me/static/media/circle-cropped.eab9224b.png"
+  },
+  {
+    "revision": "9f4b76f4d701a554dd88332f49419337",
+    "url": "/me/static/media/curriculum-vitae.9f4b76f4.png"
+  }
+]);
